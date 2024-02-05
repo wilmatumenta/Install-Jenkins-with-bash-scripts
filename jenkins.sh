3d63bf8c16816de884ba5ad4f3b923cfe6ecaa9b
wilma <94570156+wilmatumenta@users.noreply.github.com>
2024-02-05 08:58:25 +0000

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,10 +2,11 @@
 sudo apt update
 
 # install java 17
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 
 ## download and install Jenkins
 # first add the repository key to your system
+
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 ##append the Debian package repository address to the server’s sources.list:
@@ -17,5 +18,10 @@ sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update
 
 #Install Jenkins
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
+# Start Jenkins
+sudo systemctl start jenkins
+
+# Enable Jenkins to run on Boot
+sudo systemctl enable jenkins
