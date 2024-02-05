49b0cfeb725c45873a489ac3cbde776502770e7b
wilma <94570156+wilmatumenta@users.noreply.github.com>
2024-02-05 08:50:29 +0000

    Created jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update
+
+# install java 17
+sudo apt install fontconfig openjdk-17-jre
+
+## download and install Jenkins
+# first add the repository key to your system
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+##append the Debian package repository address to the server’s sources.list:
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null'
+
+# update for apt to use the new repository
+sudo apt-get update
+
+#Install Jenkins
+sudo apt-get install jenkins
+
